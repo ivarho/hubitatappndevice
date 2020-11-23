4b313a72c662a78dc08ce2f87538caf73b2da582
ivarho <holand.ivar@gmail.com>
2020-11-23 19:35:27 +0100

    Add possibility to turn off debug, trace and info

--- App/DeviceHealthMonitor.groovy
+++ App/DeviceHealthMonitor.groovy
@@ -31,8 +31,30 @@ preferences {
 		input "audioNotification", "capability.speechSynthesis", title: "Audio Notifier:"
 		input "textNotification", "capability.notification", title: "Text Notifier:"
 		input "infoPresenceSensor", "capability.presenceSensor", title: "Select precense sensor for status:"
+
+		// Debug Control
 		input "enableDebug", "bool", title: "Enable debug"
+		input "enableTrace", "bool", title: "Enable trace"
+		input "enableInfo", "bool", title: "Enable info"
 	}
+}
+
+// Housekeeping
+def logsOff() {
+	log.warn "debug logging disabled..."
+	device.updateSetting("enableDebug", [value: "false", type: "bool"])
+}
+
+def logDebug(msg) {
+	logDebug msg
+}
+
+def logTrace(msg) {
+	if (enableTrace) log.trace msg
+}
+
+def logInfo(msg) {
+	if (enableInfo) log.info msg
 }
 
 def installed() {
@@ -56,21 +78,24 @@ def initialize() {
 	schedule("0 */10 * ? * *", checkAllPrecenseSensorsPresent)
 	state.online = 1
 	state.devicesOffline = []
+
+	if (enableDebug) runIn(30*60, logsOff)
 }
 
 def presenceChangedHandler(evt) {
+	logTrace "presenceChangedHandler(evt)"
 
 	def presence = evt.getDevice().currentValue("presence")
 	def deviceName = evt.getDevice().displayName
 
-	if (enableDebug) log.debug "Presence updated to: $presence for $deviceName"
+	logDebug "Presence updated to: $presence for $deviceName"
 
 	if (infoPresenceSensor != null) {
 
 		def anyDevicesNotPresent = false;
 
 		presenceSensors.each {presenceSensor ->
-			if (enableDebug) log.debug presenceSensor.label + ": " + presenceSensor.currentValue("presence", true)
+			logDebug presenceSensor.label + ": " + presenceSensor.currentValue("presence", true)
 			if (presenceSensor.currentValue("presence") == "not present") {
 				anyDevicesNotPresent = true
 			}
@@ -95,7 +120,7 @@ def presenceChangedHandler(evt) {
 		audioNotification.speak("Dette er en informasjonsmelding: mistet kontakten med $deviceName")
 
 	} else if (presence == "present" && state.online == 0) {
-		if (enableDebug) log.debug "$onlineMessage"
+		logDebug "$onlineMessage"
 		state.online = 1
 
 		textNotification.deviceNotification("$deviceName: $onlineMessage")
@@ -103,6 +128,7 @@ def presenceChangedHandler(evt) {
 }
 
 def checkAllPrecenseSensorsPresent() {
+	logTrace "checkAllPrecenseSensorsPresent()"
 
 	if (infoPresenceSensor != null) {
 
@@ -110,7 +136,7 @@ def checkAllPrecenseSensorsPresent() {
 		def shouldWarn = false;
 
 		presenceSensors.each {presenceSensor ->
-			if (enableDebug) log.debug presenceSensor.label + ": " + presenceSensor.currentValue("presence", true)
+			logDebug presenceSensor.label + ": " + presenceSensor.currentValue("presence", true)
 			if (presenceSensor.currentValue("presence") == "not present") {
 				devicesPresent = false
 
