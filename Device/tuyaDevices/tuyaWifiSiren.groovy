8e3b616d8fc2a6ad17028cb7f40fc2e687ae6d5a
ivarho <holand.ivar@gmail.com>
2022-01-27 20:07:15 +0100

    Updated Wifi Siren to use latest tuya integration
    Signed-off-by: ivarho <holand.ivar@gmail.com>

--- Device/tuyaDevices/tuyaWifiSiren.groovy
+++ Device/tuyaDevices/tuyaWifiSiren.groovy
@@ -29,6 +29,8 @@ metadata {
 		attribute "sirenType", "string"
 		attribute "sirenLength", "number"
 
+		attribute "availableEndpoints", "String"
+
 		command "status"
 		command "setSirenType", [[name:"SirenType*", type: "NUMBER", description: "Select siren type (1-10)"]]
 		command "setSirenLength", [[name:"SirenLength*", type: "NUMBER", description: "Select the length of the siren (1-60)"]]
@@ -43,6 +45,7 @@ preferences {
 		input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
 		input name: "sirenSound", type: "enum", title: "Select sound for siren:", options : [1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10"], defaultValue: 1
 		input name: "strobeSound", type: "enum", title: "Select sound for strobe:", options : [1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10"], defaultValue: 1
+		input "tuyaProtVersion", "enum", title: "Select tuya protocol version: ", required: true, options: [31: "3.1", 33 : "3.3"]
 	}
 }
 
@@ -77,43 +80,108 @@ def setSirenLength(length) {
 }
 
 def parse(String description) {
+	if (logEnable) log.debug "Receiving message from device"
 	if (logEnable) log.debug(description)
-	if (logEnable) log.debug(description)
-	if (logEnable) log.debug "Parsing incoming message"
 
 	byte[] msg_byte = hubitat.helper.HexUtils.hexStringToByteArray(description)
 
-	String status = new String(msg_byte, "ASCII")
+	String status = new String(msg_byte, "UTF-8")
+
+	String protocol_version = ""
 
 	status = status[20..-1]
 
-	if (logEnable) log.debug status
+	if (logEnable) log.debug "Raw incoming data: " + status
 
-	if (status.startsWith("{")) {
+	if (!status.startsWith("{")) {
+		// Encrypted message incoming, decrypt first
 
-		def jsonSlurper = new groovy.json.JsonSlurper()
-		def status_object = jsonSlurper.parseText(status)
+		if (logEnable) log.debug "Encrypted message detected"
+		if (logEnable) log.debug "Bytes incoming: " + msg_byte.size()
 
+		def message_start = 0
 
-		if (status_object.dps["104"] == true) {
-			sendEvent(name: "alarm", value : "siren", isStateChange : true)
-		} else {
-			sendEvent(name: "alarm", value : "off", isStateChange : true)
+		// Find message type to determine start of message
+		def message_type = msg_byte[11].toInteger()
+
+		if (message_type == 7) {
+			// Incoming control message
+			// Find protocol version
+			byte[] ver_bytes = [msg_byte[48], msg_byte[49], msg_byte[50]]
+			protocol_version = new String(ver_bytes)
+
+			if (protocol_version == "3.1") {
+				message_start = 67
+			} else if (protocol_version == "3.3") {
+				message_start = 63
+			}
+
+		} else if (message_type == 10) {
+			// Incoming status message
+			message_start = 20
+
+			// Status messages do not contain version information, however v 3.3
+			// protocol encrypts status messages, v 3.1 does not
+			protocol_version = "3.3"
 		}
 
-		if (status_object.dps["102"] != null) {
-			// Siren type
-			sendEvent(name: "sirenType", value: status_object.dps["102"])
+		// Find end of message by looking for 0xAA55
+		def end_of_message = 0
+		for (u = message_start; u < msg_byte.size()-1; u++) {
+			if (msg_byte[u] == (byte)0xAA && msg_byte[u+1] == (byte)0x55) {
+				//msg end found
+				if (logEnable) log.debug "End of message: ${u-message_start-6}"
+				end_of_message = u-message_start-6
+				break
+			}
 		}
 
-		if (status_object.dps["103"] != null) {
-			// Siren length
-			sendEvent(name: "sirenLength", value: status_object.dps["103"])
+		// Re-assemble the bytes for decoding
+		ByteArrayOutputStream output = new ByteArrayOutputStream()
+		for (i = message_start; i < end_of_message+message_start; i++) {
+			output.write(msg_byte[i])
 		}
+
+		byte[] payload = output.toByteArray()
+
+		if (logEnable) log.debug "Assembled payload for decrypt: "+ hubitat.helper.HexUtils.byteArrayToHexString(payload)
+
+		def dec_status = ""
+
+		if (protocol_version == "3.1") {
+			dec_status = decrypt_bytes(payload, settings.localKey, true)
+		} else if (protocol_version == "3.3") {
+			dec_status = decrypt_bytes(payload, settings.localKey, false)
+		}
+
+		if (logEnable) log.debug "Decryted message: ${dec_status}"
+
+		status = dec_status
+	}
+
+	def jsonSlurper = new groovy.json.JsonSlurper()
+	def status_object = jsonSlurper.parseText(status)
+
+	sendEvent(name: "availableEndpoints", value: status_object.dps)
+
+	if (status_object.dps["104"] == true) {
+		sendEvent(name: "alarm", value : "siren", isStateChange : true)
+	} else {
+		sendEvent(name: "alarm", value : "off", isStateChange : true)
+	}
+
+	if (status_object.dps["102"] != null) {
+		// Siren type
+		sendEvent(name: "sirenType", value: status_object.dps["102"])
+	}
+
+	if (status_object.dps["103"] != null) {
+		// Siren length
+		sendEvent(name: "sirenLength", value: status_object.dps["103"])
 	}
 
 	try {
-		//interfaces.rawSocket.close()
+		interfaces.rawSocket.close()
 	} catch (e) {
 		log.error "Could not close socket: $e"
 	}
@@ -125,7 +193,7 @@ def payload()
 		"device": [
 			"status": [
 				"hexByte": "0a",
-				"command": ["devId": "", "gwId": ""]
+				"command": ["devId": "", "gwId": "", "uid":"", "t": ""]
 			],
 			"set": [
 				"hexByte": "07",
@@ -142,39 +210,43 @@ def payload()
 // Huge thank you to MrYutz for posting Groovy AES ecryption drivers for groovy
 //https://community.hubitat.com/t/groovy-aes-encryption-driver/31556
 
-// do the magic
-def encrypt (def plainText, def secret) {
-	//if (logEnable) log.debug ("Encrypting - ${plainText}","trace")
-	// this particular magic sauce pads the payload to 128 bits per chunk
-	// even though that shouldn't work with S5Padding....
+// Encrypt plain text v. 3.1 uses base64 encoding, while 3.3 does not
+def encrypt (def plainText, def secret, encodeB64=true) {
+
+	// Encryption is AES in ECB mode, pad using PKCS5Padding as needed
 	def cipher = Cipher.getInstance("AES/ECB/PKCS5Padding ")
 	SecretKeySpec key = new SecretKeySpec(secret.getBytes("UTF-8"), "AES")
 
-	// initialize the encryption and get ready for that dirty dirty magic
+	// Give the encryption engine the encryption key
 	cipher.init(Cipher.ENCRYPT_MODE, key)
-	// boom goes the dynamite
-	def result = cipher.doFinal(plainText.getBytes("UTF-8")).encodeBase64().toString()
+
+	def result = ""
+
+	if (encodeB64) {
+		result = cipher.doFinal(plainText.getBytes("UTF-8")).encodeBase64().toString()
+	} else {
+		result = cipher.doFinal(plainText.getBytes("UTF-8")).encodeHex().toString()
+	}
 
 	return result
 }
 
-// undo the magic
-def decrypt (def cypherText, def secret) {
-	//log ("Decrypting - ${cypherText}","trace")
-	// this was so much easier to get done than the encryption.  It works.  Don't touch it
-	// cereal.  you will regret it.
+// Decrypt ByteArray
+def decrypt_bytes (byte[] cypherBytes, def secret, decodeB64=false) {
+	if (logEnable) log.debug "*********** Decrypting **************"
 
-	// drop those beats..or bytes.
-	byte[] decodedBytes = cypherText.getBytes("UTF-8")
-	//log("decodedBytes:   ${decodedBytes}", "trace")
-	// no whammy no whammy no whammy.......
-	def cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
-	SecretKeySpec key = new SecretKeySpec(secret.getBytes("UTF-8"), "AES")
+	def cipher = Cipher.getInstance("AES/ECB/PKCS5Padding ")
+	SecretKeySpec key = new SecretKeySpec(secret.getBytes(), "AES")
+
 	cipher.init(Cipher.DECRYPT_MODE, key)
 
-	if (logEnable) log.debug decodedBytes.size()
-	//whammy!
-	return new String(cipher.doFinal(decodedBytes), "UTF-8")
+	if (decodeB64) {
+		cypherBytes = cypherBytes.decodeBase64()
+	}
+
+	def result = cipher.doFinal(cypherBytes)
+
+	return new String(result, "UTF-8")
 }
 
 def generateMD5(String s){
@@ -215,8 +287,8 @@ def generate_payload(command, data=null) {
 	}
 	if (json_data.containsKey("t")) {
 		Date now = new Date()
-		json_data["t"] = (now.getTime()/1000).toInteger()
-		//json_data["t"] = "1602184793"
+		json_data["t"] = (now.getTime()/1000).toInteger().toString()
+		//json_data["t"] = "1602184793" // for testing
 	}
 
 	if (data != null) {
@@ -225,17 +297,21 @@ def generate_payload(command, data=null) {
 
 	json json_data
 
+	if (logEnable) log.debug tuyaProtVersion
+
 	json_payload = groovy.json.JsonOutput.toJson(json.toString())
 	json_payload = json_payload.replaceAll("\\\\", "")
 	json_payload = json_payload.replaceFirst("\"", "")
 	json_payload = json_payload[0..-2]
 
-	if (logEnable) log.debug json_payload
+	if (logEnable) log.debug "payload before=" + json_payload
 
-	if (command == "set") {
+	ByteArrayOutputStream output = new ByteArrayOutputStream()
+
+	if (command == "set" && tuyaProtVersion == "31") {
 		encrypted_payload = encrypt(json_payload, settings.localKey)
 
-		if (logEnable) log.debug encrypted_payload
+		if (logEnable) log.debug "Encrypted payload: " + hubitat.helper.HexUtils.byteArrayToHexString(encrypted_payload.getBytes())
 
 		preMd5String = "data=" + encrypted_payload + "||lpv=" + "3.1" + "||" + settings.localKey
 
@@ -246,22 +322,34 @@ def generate_payload(command, data=null) {
 		hexdig = new String(hexdigest[8..-9].getBytes("UTF-8"), "ISO-8859-1")
 
 		json_payload = "3.1" + hexdig + encrypted_payload
+
+	} else if (tuyaProtVersion == "33") {
+		encrypted_payload = encrypt(json_payload, settings.localKey, false)
+
+		if (logEnable) log.debug encrypted_payload
+
+		if (command != "status" && command != "12") {
+			output.write("3.3".getBytes())
+			output.write("\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000".getBytes())
+			output.write(hubitat.helper.HexUtils.hexStringToByteArray(encrypted_payload))
+		} else {
+			output.write(hubitat.helper.HexUtils.hexStringToByteArray(encrypted_payload))
+		}
 	}
 
-	if (logEnable) log.debug json_payload
+	if (tuyaProtVersion == "31") {
+		output.write(json_payload.getBytes())
+	}
 
-	ByteArrayOutputStream output = new ByteArrayOutputStream()
+	if (logEnable) log.debug "payload after=" + json_payload
 
-	output.write(json_payload.getBytes())
 	output.write(hubitat.helper.HexUtils.hexStringToByteArray(payload()["device"]["suffix"]))
 
 	byte[] bff = output.toByteArray()
 
-	//if (logEnable) log.debug hubitat.helper.HexUtils.byteArrayToHexString(bff)
+	if (logEnable) log.debug hubitat.helper.HexUtils.byteArrayToHexString(bff)
 
 	postfix_payload = bff
-
-	//if (logEnable) log.debug "Postfix payload: " + postfix_payload
 
 	postfix_payload_hex_len = postfix_payload.size()
 
@@ -284,7 +372,12 @@ def generate_payload(command, data=null) {
 
 	hex_crc = Long.toHexString(crc32)
 
-	if (logEnable) log.debug "HEX crc: $hex_crc"
+	if (logEnable) log.debug "HEX crc: $hex_crc : " + hex_crc.size()/2
+
+	// Pad the CRC in case highest byte is 0
+	if (hex_crc.size() < 7) {
+		hex_crc = "00" + hex_crc
+	}
 
 	crc_bytes = hubitat.helper.HexUtils.hexStringToByteArray(hex_crc)
 
@@ -321,7 +414,9 @@ def status() {
 }
 
 def off() {
-	sendEvent(name: "switch", value : "off", isStateChange : true)
+	def buf = generate_payload("set", ["104":false])
+
+	send(buf)
 }
 
 def both() {
@@ -334,10 +429,6 @@ def siren() {
 	def buf = generate_payload("set", ["104":true])
 
 	send(buf)
-
-	// Poll for status change
-	runIn(1, status)
-	runIn(device.currentValue("sirenLength").toInteger(), afterStatus)
 }
 
 def strobe() {
@@ -346,8 +437,4 @@ def strobe() {
 	def buf = generate_payload("set", ["104":true])
 
 	send(buf)
-
-	// Poll for status change
-	runIn(1, status)
-	runIn(device.currentValue("sirenLength").toInteger(), afterStatus)
 }
