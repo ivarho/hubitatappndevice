db7434386c0cca436f018dbf4377aabb4cd3fb66
ivarho <holand.ivar@gmail.com>
2022-11-18 20:33:19 +0100

    Added a fix where the socket is forced closed on error
    Signed-off-by: ivarho <holand.ivar@gmail.com>

--- Device/tuyaDevices/tuyaGenericWindowBlind.groovy
+++ Device/tuyaDevices/tuyaGenericWindowBlind.groovy
@@ -216,6 +216,12 @@ def socketStatus(String message) {
 	if (logEnable) log.debug "*** Socket status: ${message}"
 
 	if (logEnable) log.info "Socket errors usually happen because a second message is sent before the previous is completed. Add delay between controlling several endpoints on the device."
+
+	try {
+		interfaces.rawSocket.close()
+	} catch (e) {
+		log.error "Could not close socket: $e"
+	}
 }
 
 def generate_payload(command, data=null) {
