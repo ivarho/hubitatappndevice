822284256f4269d943965f2131be163892555630
ivarho <holand.ivar@gmail.com>
2022-11-28 22:05:10 +0100

    Updated TibberLiveConsumption to use new connection protocol
    Updated to use the graphql-transport-ws as Tibber is discontinuing the
    previous interface.
    
    Signed-off-by: ivarho <holand.ivar@gmail.com>

--- Device/TibberLiveConsumption.groovy
+++ Device/TibberLiveConsumption.groovy
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ *
  * Change log:
+ * - Added support for the new webSocket API graphql-transport-ws
  * - Added a motion detector output as indication of over consumption
  * - Added setting for controlling run time, i.e. continuous, every 5 min etc.
  *    Auto reconnect added.
@@ -38,13 +40,14 @@ metadata {
 
 		command "connectSocket"
 		command "closeSocket"
+		command "querySubscriptionURL"
 	}
 
 	preferences {
 		input name: "tibber_apikey", type: "password", title: "API Key", description: "Enter the Tibber API key.<p><i>This can be found on https://developer.tibber.com/explorer. Sign in and click Load Personal Token.</i></p>", required: true, displayDuringSetup: true
 		input name: "tibber_homeId", type: "text", title: "homeId", description: "Enter the Tibber homeId: <p><i>This can be found on https://developer.tibber.com/explorer. Open the Real time subscription example, homeId should be on the left.</i></p>", required: true, displayDuringSetup: true
 		input name: "threshold", type: "number", title: "Limit hour average to:", default: 5000
-		input name: "run_config", type: "enum", title: "Configure run schedule:", options: [1:"Continuous without auto reconnect", 2:"Continuous with auto reconnect", 3: "Every 3 min", 5: "Every 5 min", 10: "Every 10 min", 15: "Every 15 min", 20: "Every 20 min", 30: "Every 30 min"]
+		input name: "run_config", type: "enum", title: "Configure run schedule:", options: [1:"Continuous without auto reconnect ~7000 evt/hour", 2:"Continuous with auto reconnect ~7000 evt/hour", 3: "Every 3 min ~80 evt/hour", 5: "Every 5 min  ~48 evt/hour", 10: "Every 10 min ~24 evt/hour", 15: "Every 15 min ~16 evt/hour", 20: "Every 20 min ~12 evt/hour", 30: "Every 30 min ~8 evt/hour"]
 
 		input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
 	}
@@ -56,7 +59,34 @@ def parse(description) {
 	def jsonSlurper = new JsonSlurper()
 	def object = jsonSlurper.parseText(description)
 
-	if (object.type == "data") {
+	if (object.type == "connection_ack") {
+		if(logEnable) log.debug "Connection accepted"
+
+		state.error_count = 0;
+
+		sub = '{"type": "subscribe", "id": "'+ state.uuid +'", "payload": {"query": "subscription{liveMeasurement(homeId:\\"'+ tibber_homeId +'\\"){timestamp\\r\\npower\\r\\naccumulatedConsumptionLastHour}}"}}'
+
+		interfaces.webSocket.sendMessage(sub)
+	}
+
+	if (object.type == "error") {
+		log.error "Connection error with Tibber"
+
+		querySubscriptionURL()
+
+		if (state.error_count == null) {
+			state.error_count = 1;
+		} else {
+			state.error_count = state.error_count + 1
+		}
+
+		// Safest thing to do is to unschedule
+		if (state.error_count > 10) {
+			unschedule()
+		}
+	}
+
+	if (object.type == "next") {
 
 		def power = object.payload.data.liveMeasurement.power
 		def accumulatedConsumptionLastHour = object.payload.data.liveMeasurement.accumulatedConsumptionLastHour
@@ -91,7 +121,7 @@ def parse(description) {
 }
 
 def webSocketStatus(String message) {
-	log.debug "webSocketStatus: $message"
+	if(logEnable) log.debug "webSocketStatus: $message"
 
 	if (message == "status: open") {
 		sendEvent(name:"switch", value:"on")
@@ -110,13 +140,48 @@ def webSocketStatus(String message) {
 	}
 }
 
-def connectSocket() {
-	auth = '{"type":"connection_init","payload":"token='+ tibber_apikey +'"}'
-	sub = '{"type": "start", "id": "1", "payload": {"query": "subscription{liveMeasurement(homeId:\\"'+ tibber_homeId +'\\"){timestamp\\r\\npower\\r\\naccumulatedConsumptionLastHour}}"}}'
+def graphQLApiQuery(){
+	return '{"query":"{ viewer { websocketSubscriptionUrl } }"}'
+}
 
-	interfaces.webSocket.connect("wss://api.tibber.com/v1-beta/gql/subscriptions", pingInterval:-1, headers: ["Authorization":"{$tibber_apikey}", "Sec-WebSocket-Protocol": "graphql-ws"])
+def querySubscriptionURL() {
+	def params = [
+		uri: "https://api.tibber.com/v1-beta/gql",
+		headers: ["Content-Type": "application/json" , "Authorization": "Bearer $tibber_apikey"],
+		body: graphQLApiQuery()
+		]
+
+	try {
+		httpPostJson(params) { resp ->
+			if (resp.status == 200) {
+				if(logEnable) log.debug resp.data
+				state.subscriptionURL = resp.data.data.viewer.websocketSubscriptionUrl
+			}
+		}
+	} catch (e) {
+		log.error "something went wrong: $e"
+	}
+}
+
+def connectSocket() {
+	auth = '{"type":"connection_init","payload":{"token":"'+ tibber_apikey +'"}}'
+
+	if (state.subscriptionURL == null) {
+		querySubscriptionURL()
+	}
+
+	interfaces.webSocket.connect(state.subscriptionURL, pingInterval:20, headers: ["Sec-WebSocket-Protocol": "graphql-transport-ws", "User-Agent":"Hubitat/1 iholand-TibberLiveConsumption/1.0-alpha"])
 	interfaces.webSocket.sendMessage(auth)
-	interfaces.webSocket.sendMessage(sub)
+
+}
+
+def endConnection() {
+	completeMessage = '{"type":"complete", "id":"'+ state.uuid +'"}'
+
+	if(logEnable) log.debug completeMessage
+
+	interfaces.webSocket.sendMessage(completeMessage)
+
 }
 
 def updated(settings) {
@@ -125,6 +190,12 @@ def updated(settings) {
 	closeSocket()
 
 	unschedule()
+
+	if (!state.uuid) {
+		state.uuid = UUID.randomUUID().toString()
+	}
+
+	querySubscriptionURL()
 
 	if (run_config.toInteger() > 2) {
 		//Schedule run
@@ -138,7 +209,9 @@ def updated(settings) {
 }
 
 def closeSocket() {
-	log.debug "closeSocket"
+	if(logEnable) log.debug "closeSocket"
+	endConnection()
+
 	interfaces.webSocket.close()
 }
 
