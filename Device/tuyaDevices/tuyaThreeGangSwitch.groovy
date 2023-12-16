f7927a745ad5778e3e31d7e9fa0fbac0224c1846
ivarho <holand.ivar@gmail.com>
2023-12-16 13:47:57 +0100

    Fixed device drivers copyright year

--- Device/tuyaDevices/tuyaThreeGangSwitch.groovy
+++ Device/tuyaDevices/tuyaThreeGangSwitch.groovy
@@ -1,7 +1,7 @@
 /**
  * IMPORT URL: https://raw.githubusercontent.com/ivarho/hubitatappndevice/master/Device/tuyaDevices/tuyaThreeGangSwitch.groovy
  *
- * Copyright 2022 Ivar Holand
+ * Copyright 2022-2023 Ivar Holand
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
